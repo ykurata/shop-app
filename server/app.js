9de0d9fa48f2590d4ab0c4f8c8bb7666ba22575b
ykurata <yasukokurata@gmail.com>
2020-03-20 18:44:38 -0400

    initial commit

---
+++ server/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server up and running on port ${port} !`));
+
+module.exports = app;
