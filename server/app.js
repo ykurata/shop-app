a16018d0adebd588c2bbf1154c13b795b6ca5ccd
ykurata <yasukokurata@gmail.com>
2020-03-22 13:33:00 -0400

    update register route

--- server/app.js
+++ server/app.js
@@ -2,16 +2,25 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
 const passport = require("passport");
-
 const app = express();
+
+// Import routes 
+const user = require("./routes/user");
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require("./config/passport")(passport);
+
+// Routes 
+app.use("/user", user);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
--- server/models/user.js
+++ server/models/user.js
@@ -1,11 +1,20 @@
 'use strict';
+const bcrypt = require('bcryptjs');
 
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     username: DataTypes.STRING,
     email: DataTypes.STRING,
     password: DataTypes.STRING
-  }, {});
+  }, {
+    freezeTableName: true,
+    tableName: 'Users',
+  });
+  User.beforeSave((user, options) => {
+    if (user.changed('password')) {
+      user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
+    }
+  });
   User.associate = function(models) {
     // associations can be defined here
   };
