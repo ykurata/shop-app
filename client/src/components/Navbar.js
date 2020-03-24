892b8710d75a60220ed555ec174da2f0a374d2a3
ykurata <yasukokurata@gmail.com>
2020-03-24 18:13:54 -0400

    updated Navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -11,38 +11,47 @@ class Navbar extends Component {
   render() {
     return (
       <div>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <a className="navbar-brand" href="#">Navbar</a>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul className="navbar-nav">
-              <li className="nav-item active">
-                <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Features</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Pricing</a>
-              </li>
-              <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  Dropdown link
-                </a>
-                <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                  <a className="dropdown-item" href="#">Action</a>
-                  <a className="dropdown-item" href="#">Another action</a>
-                  <a className="dropdown-item" href="#">Something else here</a>
-                </div>
-              </li>
+        <nav className="navbar navbar-expand-md navbar-light bg-light">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="/">Navbar</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavDropdown">
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item dropdown">
+                  <button class="btn btn-info my-2 my-sm-0" type="submit">Post Item</button>
+                </li>  
+                <li className="nav-item dropdown">
+                  <a className="nav-link dropdown-toggle" href="/" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Categories
+                  </a>
+                  <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <a className="dropdown-item" href="/">Action</a>
+                    <a className="dropdown-item" href="/">Another action</a>
+                    <a className="dropdown-item" href="/">Something else here</a>
+                  </div>
+                </li>
 
-              <form class="form-inline">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-              </form>
-            </ul>
+                {/* Search form  */}
+                <form className="form-inline">
+                  <input type="text" class="form-control" placeholder="Search" aria-label="Search" aria-describedby="basic-addon2" />
+                  <div className="input-group-append">
+                    <button className="btn btn-secondary" type="submit">
+                      <i className="fa fa-search"></i>
+                    </button>
+                  </div>
+                </form>
+
+                <li className="nav-item">
+                  <a className="nav-link" href="/">Log In</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/">Sign Up</a>
+                </li>
+              </ul>
+              
+            </div>
           </div>
         </nav>
       </div>
