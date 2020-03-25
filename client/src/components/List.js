d735ff4041ce3543e90e0673d3ce652603321ff3
ykurata <yasukokurata@gmail.com>
2020-03-25 12:34:55 -0400

    create list itme's card

--- client/src/components/List.js
+++ client/src/components/List.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import { withStyles } from '@material-ui/core/styles';
 
 import Navbar from "./Navbar";
+
+const ListStyles = theme => ({
+  avatar: {
+    height: 150,
+    width: 150,
+    textDecoration: "none",
+  }
+});
 
 class List extends Component {
   constructor(props) {
@@ -11,18 +21,24 @@ class List extends Component {
   };
 
   render() {
+    const { classes } = this.props;
     return (
       <div>
         <Navbar></Navbar>
         
         <div className="container item-list">
           <div className="card">
-            <h5 className="card-header">Featured</h5>
-            <div className="card-body">
-              <h5 className="card-title">Special title treatment</h5>
-              <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-              <a href="/" className="btn btn-primary">Go somewhere</a>
+            <div className="card-body row">
+              <div className="col-3">
+                <Avatar className={classes.avatar} variant='square'></Avatar>
+              </div>
+              <div className="title col-9">
+                <h5>Item's Title</h5>
+                <p className="date">25/03/2020</p>
+                <p className="description">Bacon ipsum dolor amet kevin short loin buffalo drumstick fatback venison ground round spare ribs swine rump kielbasa shankle capicola alcatra leberkas. Bresaola alcatra short ribs, pig tail biltong tenderloin venison beef ribs capicola meatloaf chicken. Chislic ham salami filet mignon spare ribs prosciutto. Biltong drumstick turkey ham spare ribs, picanha pig meatball pancetta shankle chuck kielbasa filet mignon tri-tip landjaeger. Burgdoggen meatloaf prosciutto shank boudin chuck, ribeye pork chop. Tenderloin andouille beef buffalo sausage pork loin doner.</p>
+              </div>
             </div>
+
           </div>
         </div>
       </div>
@@ -30,4 +46,4 @@ class List extends Component {
   }
 }
 
-export default List;
+export default withStyles(ListStyles)(List);
