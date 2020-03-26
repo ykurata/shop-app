8923027d6b8083e468c0f9a6aea3f119ca53e183
ykurata <yasukokurata@gmail.com>
2020-03-26 15:05:26 -0400

    add price tag

--- client/src/components/List.js
+++ client/src/components/List.js
@@ -25,7 +25,8 @@ class List extends Component {
                 <div className="no-image text-center"><i className="fas fa-image fa-5x"></i></div>
               </div>
               <div className="title col-lg-10 col-md-10">
-                <h5>Item's Title</h5>
+                <h5 className="item-price">$100.00</h5>
+                <h5 className="item-title">Item's Title</h5>
                 <p className="date">25/03/2020</p>
                 <p className="description">Bacon ipsum dolor amet kevin short loin buffalo drumstick fatback venison ground round spare ribs swine rump kielbasa shankle capicola alcatra leberkas. Bresaola alcatra short ribs, pig tail biltong tenderloin venison beef ribs capicola meatloaf chicken. Chislic ham salami filet mignon spare ribs prosciutto. Biltong drumstick turkey ham spare ribs, picanha pig meatball pancetta shankle chuck kielbasa filet mignon tri-tip landjaeger. Burgdoggen meatloaf prosciutto shank boudin chuck, ribeye pork chop. Tenderloin andouille beef buffalo sausage pork loin doner.</p>
               </div>
