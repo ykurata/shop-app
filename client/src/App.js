9821a8e8be8e30b5de9df8214064dfa1f1539418
ykurata <yasukokurata@gmail.com>
2020-03-31 13:02:31 -0400

    add PrivateRoute

--- client/src/App.js
+++ client/src/App.js
@@ -6,16 +6,17 @@ import Signup from "./components/Signup";
 import List from "./components/List";
 import Detail from "./components/Detail";
 import Form from "./components/Form";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
+        <PrivateRoute path="/form" component={Form} /> 
         <Route exact path="/" component={List} />
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
         <Route path="/detail" component={Detail} /> 
-        <Route path="/form" component={Form} /> 
       </Switch>
     </BrowserRouter>
   );
