c011a473c8c8e48737b5c6100b06ad3a8762d4b8
ykurata <yasukokurata@gmail.com>
2020-04-01 11:41:23 -0400

    updated navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,11 +4,83 @@ class Navbar extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      token: localStorage.getItem("jwtToken")
     };
   }
 
+  logOut = e => {
+    e.preventDefault();
+    localStorage.clear();
+    window.location.href="/"
+  }
+
   render() {
+    let navList;
+
+    if (this.state.token) {
+      navList = <ul className="navbar-nav ml-auto">
+                  <li className="nav-item dropdown">
+                    <a className="btn btn-large btn-primary my-2 my-sm-0" href="/create">Post Item</a>
+                  </li>  
+                  <li className="nav-item dropdown">
+                    <a className="nav-link dropdown-toggle" href="/" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      Categories
+                    </a>
+                    <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                      <a className="dropdown-item" href="/">Action</a>
+                      <a className="dropdown-item" href="/">Another action</a>
+                      <a className="dropdown-item" href="/">Something else here</a>
+                    </div>
+                  </li>
+                  {/* Search form  */}
+                  <form className="form-inline">
+                      <input type="text" className="form-control" placeholder="Search" aria-label="Search" aria-describedby="basic-addon2" />
+                      <div className="input-group-append">
+                        <button className="btn btn-secondary" type="submit">
+                          <i className="fa fa-search"></i>
+                        </button>
+                      </div>
+                  </form>
+
+                  <li className="nav-item">
+                    <a className="nav-link" href="/logout" onClick={this.logOut}>Log Out</a>
+                  </li>
+                </ul>
+    } else {
+      navList = <ul className="navbar-nav ml-auto">
+                  <li className="nav-item dropdown">
+                    <a className="btn btn-large btn-primary my-2 my-sm-0" href="/create">Post Item</a>
+                  </li>  
+                  <li className="nav-item dropdown">
+                    <a className="nav-link dropdown-toggle" href="/" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      Categories
+                    </a>
+                    <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                      <a className="dropdown-item" href="/">Action</a>
+                      <a className="dropdown-item" href="/">Another action</a>
+                      <a className="dropdown-item" href="/">Something else here</a>
+                    </div>
+                  </li>
+                  {/* Search form  */}
+                  <form className="form-inline">
+                      <input type="text" className="form-control" placeholder="Search" aria-label="Search" aria-describedby="basic-addon2" />
+                      <div className="input-group-append">
+                        <button className="btn btn-secondary" type="submit">
+                          <i className="fa fa-search"></i>
+                        </button>
+                      </div>
+                  </form>
+
+                  <li className="nav-item">
+                    <a className="nav-link" href="/login">Log In</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="/signup">Sign Up</a>
+                  </li>
+                </ul>
+    }
+
+
     return (
       <div>
         <nav className="navbar navbar-expand-md navbar-light bg-light">
@@ -18,39 +90,9 @@ class Navbar extends Component {
               <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNavDropdown">
-              <ul className="navbar-nav ml-auto">
-                <li className="nav-item dropdown">
-                  <a className="btn btn-large btn-primary my-2 my-sm-0" href="/create">Post Item</a>
-                </li>  
-                <li className="nav-item dropdown">
-                  <a className="nav-link dropdown-toggle" href="/" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Categories
-                  </a>
-                  <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                    <a className="dropdown-item" href="/">Action</a>
-                    <a className="dropdown-item" href="/">Another action</a>
-                    <a className="dropdown-item" href="/">Something else here</a>
-                  </div>
-                </li>
 
-                {/* Search form  */}
-                <form className="form-inline">
-                  <input type="text" className="form-control" placeholder="Search" aria-label="Search" aria-describedby="basic-addon2" />
-                  <div className="input-group-append">
-                    <button className="btn btn-secondary" type="submit">
-                      <i className="fa fa-search"></i>
-                    </button>
-                  </div>
-                </form>
-
-                <li className="nav-item">
-                  <a className="nav-link" href="/login">Log In</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/signup">Sign Up</a>
-                </li>
-              </ul>
               
+              {navList}
             </div>
           </div>
         </nav>
