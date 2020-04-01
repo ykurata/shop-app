76adec1b836b2e8eaf89e94ca95c97d4375beff9
ykurata <yasukokurata@gmail.com>
2020-04-01 17:25:01 -0400

    create onSubmit function in create form component

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,15 +1,44 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import phone2 from "../images/phone.jpg"
 
 import Navbar from "./Navbar";
+import Axios from 'axios';
 
 class Form extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      title: "",
+      category: "",
+      price: "",
+      description: "",
+      token: localStorage.getItem("jwtToken"),
     };
   };
+
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+  
+  onSubmit = e => {
+    e.preventDefault();
+
+    const newItem = {
+      title: this.state.title,
+      category: this.state.category,
+      price: this.state.price,
+      description: this.state.description
+    }
+
+    axios.post("/item", newItem, { headers: { Authorization: `Bearer ${this.state.token}`}})
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
 
   render() {
     return (
@@ -17,14 +46,14 @@ class Form extends Component {
         <Navbar></Navbar>
 
         <div className="form">
-          <form>
+          <form onSubmit={this.onSubmit}>
             <div className="form-group">
               <label>Title</label>
-              <input type="text" className="form-control" id="itemTitle" placeholder="Item's Title"/>
+              <input type="text" onChange={this.onChange} className="form-control" name="title" id="title" placeholder="Item's Title"/>
             </div>
             <div className="form-group">
               <label>Category</label>
-              <select className="form-control" id="category">
+              <select className="form-control" id="category" name="category" onChange={this.onChange}>
                 <option>Phone/Laptop</option>
                 <option>Clothing</option>
                 <option>Books</option>
@@ -38,7 +67,7 @@ class Form extends Component {
                 <div className="input-group-prepend">
                   <span className="input-group-text">$</span>
                 </div>
-                <input type="text" className="form-control" aria-label="Amount (to the nearest dollar)" />
+                <input type="text" name="price" onChange={this.onChange} className="form-control" aria-label="Amount (to the nearest dollar)" />
                 <div className="input-group-append">
                   <span className="input-group-text">.00</span>
                 </div>
@@ -46,7 +75,7 @@ class Form extends Component {
             </div>
             <div className="form-group">
               <label>Description</label>
-              <textarea className="form-control" id="description" rows="5"></textarea>
+              <textarea className="form-control"  onChange={this.onChange} id="description" name="description" rows="5"></textarea>
             </div>
             <button type="button" className="btn btn-primary btn-lg btn-block">Submit</button>
           </form>
