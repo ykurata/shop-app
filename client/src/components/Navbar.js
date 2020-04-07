fd4289debf7f75d01297eb623f870636fab6f891
ykurata <yasukokurata@gmail.com>
2020-04-07 11:18:59 -0400

    update navbar with avatar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import phone2 from "../images/phone.jpg"
 
 class Navbar extends Component {
   constructor(props) {
@@ -15,90 +16,60 @@ class Navbar extends Component {
   }
 
   render() {
-    let navList;
-
-    if (this.state.token) {
-      navList = <ul className="navbar-nav ml-auto">
-                  <li className="nav-item dropdown">
-                    <a className="btn btn-large btn-primary my-2 my-sm-0" href="/create">Post Item</a>
-                  </li>  
-                  <li className="nav-item dropdown">
-                    <a className="nav-link dropdown-toggle" href="/" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                      Categories
-                    </a>
-                    <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                      <a className="dropdown-item" href="/">Action</a>
-                      <a className="dropdown-item" href="/">Another action</a>
-                      <a className="dropdown-item" href="/">Something else here</a>
-                    </div>
-                  </li>
-                  {/* Search form  */}
-                  <form className="form-inline">
-                      <input type="text" className="form-control" placeholder="Search" aria-label="Search" aria-describedby="basic-addon2" />
-                      <div className="input-group-append">
-                        <button className="btn btn-secondary" type="submit">
-                          <i className="fa fa-search"></i>
-                        </button>
-                      </div>
-                  </form>
-
-                  <li className="nav-item">
-                    <a className="nav-link" href="/logout" onClick={this.logOut}>Log Out</a>
-                  </li>
-                </ul>
-    } else {
-      navList = <ul className="navbar-nav ml-auto">
-                  <li className="nav-item dropdown">
-                    <a className="btn btn-large btn-primary my-2 my-sm-0" href="/create">Post Item</a>
-                  </li>  
-                  <li className="nav-item dropdown">
-                    <a className="nav-link dropdown-toggle" href="/" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                      Categories
-                    </a>
-                    <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                      <a className="dropdown-item" href="/">Action</a>
-                      <a className="dropdown-item" href="/">Another action</a>
-                      <a className="dropdown-item" href="/">Something else here</a>
-                    </div>
-                  </li>
-                  {/* Search form  */}
-                  <form className="form-inline">
-                      <input type="text" className="form-control" placeholder="Search" aria-label="Search" aria-describedby="basic-addon2" />
-                      <div className="input-group-append">
-                        <button className="btn btn-secondary" type="submit">
-                          <i className="fa fa-search"></i>
-                        </button>
-                      </div>
-                  </form>
-
-                  <li className="nav-item">
-                    <a className="nav-link" href="/login">Log In</a>
-                  </li>
-                  <li className="nav-item">
-                    <a className="nav-link" href="/signup">Sign Up</a>
-                  </li>
-                </ul>
-    }
-
-
+    
     return (
-      <div>
-        <nav className="navbar navbar-expand-md navbar-light bg-light">
-          <div className="container-fluid">
-            <a className="navbar-brand" href="/">Navbar</a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNavDropdown">
+     <div>
+      <nav className="navbar navbar-expand-md navbar-light bg-light">
+        <a className="navbar-brand" href="#">Navbar</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-555"
+          aria-controls="navbarSupportedContent-555" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent-555">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="/create">Post Item
+                <span className="sr-only">(current)</span>
+              </a>
+            </li>
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" id="navbarDropdownMenuLink-555" data-toggle="dropdown"
+                aria-haspopup="true" aria-expanded="false">Category
+              </a>
+              <div className="dropdown-menu dropdown-secondary" aria-labelledby="navbarDropdownMenuLink-555">
+                <a className="dropdown-item" href="/">Action</a>
+                <a className="dropdown-item" href="/">Another action</a>
+                <a className="dropdown-item" href="/">Something else here</a>
+              </div>
+            </li>
 
-              
-              {navList}
-            </div>
-          </div>
-        </nav>
-      </div>
-    )
-  }
+            <form className="form-inline">
+              <div className="md-form my-0">
+                <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
+              </div>
+            </form>
+
+          </ul>
+          <ul className="navbar-nav ml-auto nav-flex-icons">
+            <li className="nav-item avatar dropdown">
+              <a className="nav-link dropdown-toggle" id="navbarDropdownMenuLink-55" data-toggle="dropdown"
+                aria-haspopup="true" aria-expanded="false">
+                <img src="https://mdbootstrap.com/img/Photos/Avatars/avatar-2.jpg" className="rounded-circle z-depth-0 navbar-img"
+                  alt="avatar image" />
+              </a>
+              <div className="dropdown-menu dropdown-menu-lg-right dropdown-secondary"
+                aria-labelledby="navbarDropdownMenuLink-55">
+                <a className="dropdown-item" href="/">Action</a>
+                <a className="dropdown-item" href="/">Another action</a>
+                <a className="dropdown-item" href="/">Something else here</a>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>  
+    );
+  }  
 }
 
 export default Navbar;
