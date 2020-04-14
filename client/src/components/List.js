8b9f3acc30534cb92ab901ccc22748be94704c0d
ykurata <yasukokurata@gmail.com>
2020-04-14 15:00:23 -0400

    split list page by grid

--- client/src/components/List.js
+++ client/src/components/List.js
@@ -74,22 +74,10 @@ class List extends Component {
       <div>
         <Navbar></Navbar>
         {/* display number of items */}
-        <div className="container item-list">
-
-          {/* Search form */}
-          <div className="search row"> 
-            <div className="col-lg-4">
-            <select onChange={this.onChange} name="search" className="browser-default custom-select mb-4">
-              <option value="">Search from category</option>
-              <option value="Phone/Laptop">Phone/Laptop</option>
-              <option value="Clothing">Clothing</option>
-              <option value="Books">Books</option>
-              <option value="Music">Music</option>
-              <option value="Furniture">Furniture</option>
-              <option value="Camera">Camera</option>
-            </select>
-            </div>   
-            <div className="col-lg-8">
+        <div className="container-fluid item-list">
+          <div className="row list-outer">
+            {/* Search input and select form category */}
+            <div className="col-lg-3">
               <div className="input-group md-form form-sm form-1 pl-0 mb-5 search-form">
                 <div className="input-group-prepend">
                   <span className="input-group-text purple lighten-3" id="basic-text1"><i className="fas fa-search text-white"
@@ -97,36 +85,45 @@ class List extends Component {
                 </div>
                 <input onChange={this.onChange} name="search" value={this.state.search} className="form-control my-0 py-1" type="text" placeholder="Search by category, item..." aria-label="Search" />
               </div>
+              <select onChange={this.onChange} name="search" className="browser-default custom-select mb-4">
+                <option value="">Search from category</option>
+                <option value="Phone/Laptop">Phone/Laptop</option>
+                <option value="Clothing">Clothing</option>
+                <option value="Books">Books</option>
+                <option value="Music">Music</option>
+                <option value="Furniture">Furniture</option>
+                <option value="Camera">Camera</option>
+              </select>
             </div>
-          </div>
-          
+            <div className="col-lg-9">
+              {filteredItems.length > 0 ? (
+                <p>Showing {filteredItems.length} items</p>
+              ) : (
+                null
+              )} 
 
-          {filteredItems.length > 0 ? (
-            <p>Showing {filteredItems.length} items</p>
-          ) : (
-            null
-          )} 
+              <div className="list-group">
+                {/* display message if there is no items  */}
+                {filteredItems.length === 0 && this.state.loading === true ? (
+                  <div className="text-center mt-5">
+                    <h5>No Items</h5>
+                  </div>
+                ) : (
+                  null
+                )}
 
-          <div className="list-group">
-            {/* display message if there is no items  */}
-            {filteredItems.length === 0 && this.state.loading === true ? (
-              <div className="text-center mt-5">
-                <h5>No Items</h5>
+                {/* Loading Message */}
+                {this.state.loading === false ? (
+                  <div className="text-center mt-5">
+                    <h5>Loading Items...</h5>
+                  </div>  
+                ) : (
+                  null
+                )}
+
+                {items}
               </div>
-            ) : (
-              null
-            )}
-
-            {/* Loading Message */}
-            {this.state.loading === false ? (
-              <div className="text-center mt-5">
-                <h5>Loading Items...</h5>
-              </div>  
-            ) : (
-              null
-            )}
-
-            {items}
+            </div>
           </div>
         </div>
       </div>
