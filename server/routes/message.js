63b95d23a31dc09b45041142b34c9e516aa09561
ykurata <yasukokurata@gmail.com>
2020-04-17 17:29:51 -0400

    delete Message model

--- server/routes/message.js
+++ server/routes/message.js
@@ -5,4 +5,16 @@ const authentication = require("./middleware/auth");
 
 const Message = require('../models').Message;
 
+
+// Post a new message 
+router.post("/:id", authentication, (req, res) => {
+  Message.create({
+    itemId: req.params.id,
+    senderId: req.user,
+    recieverId: req.body.recieverId,
+    text: req.body.text
+  })
+})
+
+
 module.exports = router;
