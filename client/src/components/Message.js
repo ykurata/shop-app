68c1e824dbb1fcead0befe2f6ef7ac7cd41c8696
ykurata <yasukokurata@gmail.com>
2020-04-18 15:53:39 -0400

    add checkbox in message list

--- client/src/components/Message.js
+++ client/src/components/Message.js
@@ -24,20 +24,21 @@ class Message extends Component {
         <div className="container item-list">
           
             <div className="list-group">
-              <Link to="/" className="card card-link message-list-group-item" >
-                {/* <Link to="/" className="card-link"> */}
-                  <div className="card-body row message-card-row">
-                    <div className="col-lg-2 col-md-2 col-sm-2">
-                        {/* <div className="no-image text-center"><i className="fas fa-image fa-5x"></i></div> */}
-                      <img src={phone} alt="..." className="rounded message-list-item-img" />
-                    </div>
-                    <div className="col-lg-10 col-md-10 col-sm-10">
-                      <h6>iPhone 11</h6>
-                      <p className="date"><Moment format="MM/DD/YYYY">Date</Moment></p>
-                      <p className="description">Bacon ipsum dolor amet buffalo frankfurter spare ribs fjsbvjbsk nsjdvns  vjsdknlvsjdlsjdlsdblsjdlsbdvlsjb.</p>
-                    </div>
+              <Link to="/" className="card message-card" >
+                <div className="card-body row message-card-row">
+                  <div className="col-lg-1 col-md-1 text-center">
+                    <input type="checkbox" className="form-check-input m-auto" id="exampleCheck1" />
                   </div>
-                {/* </Link>    */}
+                  <div className="col-lg-2 col-md-2 col-sm-2">
+                      {/* <div className="no-image text-center"><i className="fas fa-image fa-5x"></i></div> */}
+                    <img src={phone} alt="..." className="rounded message-list-item-img" />
+                  </div>
+                  <div className="col-lg-9 col-md-9 col-sm-9">
+                    <h6>iPhone 11</h6>
+                    <p className="date"><Moment format="MM/DD/YYYY">Date</Moment></p>
+                    <p className="description">Bacon ipsum dolor amet buffalo frankfurter spare ribs fjsbvjbsk nsjdvns  vjsdknlvsjdlsjdlsdblsjdlsbdvlsjb.</p>
+                  </div>
+                </div>
               </Link>
             </div>
         
