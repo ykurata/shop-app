ffafa910656d658b4fceb3d8aed4a1ad4cefc9a6
ykurata <yasukokurata@gmail.com>
2020-04-18 15:42:38 -0400

    updated list card

--- client/src/components/Message.js
+++ client/src/components/Message.js
@@ -24,21 +24,21 @@ class Message extends Component {
         <div className="container item-list">
           
             <div className="list-group">
-              <div className="card list-group-item" >
-                <Link to="/" className="card-link">
-                  <div className="card-body row">
-                    <div className="col-lg-3 col-md-3 col-sm-2">
+              <Link to="/" className="card card-link message-list-group-item" >
+                {/* <Link to="/" className="card-link"> */}
+                  <div className="card-body row message-card-row">
+                    <div className="col-lg-2 col-md-2 col-sm-2">
                         {/* <div className="no-image text-center"><i className="fas fa-image fa-5x"></i></div> */}
-                      <img src={phone} alt="..." className="rounded list-item-img" />
+                      <img src={phone} alt="..." className="rounded message-list-item-img" />
                     </div>
-                    <div className="col-lg-9 col-md-9 col-sm-10">
-                      <h5 className="item-title">iPhone 11</h5>
+                    <div className="col-lg-10 col-md-10 col-sm-10">
+                      <h6>iPhone 11</h6>
                       <p className="date"><Moment format="MM/DD/YYYY">Date</Moment></p>
                       <p className="description">Bacon ipsum dolor amet buffalo frankfurter spare ribs fjsbvjbsk nsjdvns  vjsdknlvsjdlsjdlsdblsjdlsbdvlsjb.</p>
                     </div>
                   </div>
-                </Link>   
-              </div>
+                {/* </Link>    */}
+              </Link>
             </div>
         
         </div>
