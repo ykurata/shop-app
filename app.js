1d449a724cd0755e2f1644a2dd806618f6f2ed30
ykurata <yasukokurata@gmail.com>
2020-05-07 23:24:50 -0400

    updated app.js

--- app.js
+++ app.js
@@ -39,16 +39,6 @@ app.use("/message", message);
 // Set up cors
 app.use(cors());
 
-// Serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
-  });
-}
-
 // send 404 if no other route matched
 app.use((req, res) => {
   res.status(404).json({
