65680e3962a50d7667d78e5dc1b12663e668c4ae
ykurata <yasukokurata@gmail.com>
2020-05-19 23:30:22 -0400

    added MessageCard component

--- client/src/pages/Message.js
+++ client/src/pages/Message.js
@@ -1,54 +1,19 @@
 import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
-import Moment from 'react-moment';
 
 import { MessageContext } from '../contexts/MessageContext';
 import { UserContext } from '../contexts/UserContext';
 import Navbar from "../components/Navbar";
 import LoginUserCard from '../components/LoginUserCard';
+import MessageCard from '../components/MessageCard';
 
 const Message = (props) => {
   const { user } = useContext(UserContext);
-  const { conversations, loading, deleteConversation } = useContext(MessageContext);
+  const { conversations, loading } = useContext(MessageContext);
   
   let conversationList;
 
   conversationList = conversations.map(con => (
-    <Link to={`/message-detail/${con.id}`} key={con.id} id={con.id} className="card item-card message-card" >
-      <div className="row message-card-row" id={con.id}>
-        <div className="col-lg-2 col-md-2 col-sm-2 col-2 message-image">
-          {con.Item.image ? (
-            <img src={con.Item.image[0]} alt="..." className="rounded message-list-item-img" />
-          ) : (
-            <div className="no-image text-center"><i className="fas fa-image fa-3x icon-image"></i></div>
-          )}  
-        </div>
-        <div className="col-lg-10 col-md-10 col-md-10 col-10">
-          
-          <div className="row message-inside-row">
-            <div className="col-lg-9 col-md-9 col-sm-9 col-9">
-              <p className="message-item-title"><b>{con.Item.name}</b></p> 
-            </div>
-            <div className="col-lg-2 col-md-2 col-sm-2 col-3 p-0">
-              <p className="message-date" ><Moment fromNow ago>{con.createdAt}</Moment>&nbsp;ago</p>
-              <button 
-                id={con.id} 
-                className="btn btn-link delete-conversation pt-3"
-                onClick={(e) => { if (window.confirm('Are you sure you wish to delete this item?')) deleteConversation(con.id) } }
-              ><i className="far fa-trash-alt"></i>&nbsp;Delete
-              </button>
-            </div>
-          </div>
-          {con.Messages.length > 0 ? (
-            <p className="text mb-2">{con.Messages[con.Messages.length-1].text}</p>
-          ) : (
-            null
-          )}
-          
-          <p className="message-username">{con.Item.User.username}</p>
-        </div>
-      </div>
-    </Link>
+    <MessageCard data={con} key={con.id}/>
   ));
 
   return (
