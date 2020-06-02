84d466b1e0473b2d784ede1de14b0aaf0df490cf
ykurata <yasukokurata@gmail.com>
2020-06-02 12:47:05 -0400

    added text modal on navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 
 import { UserContext } from '../contexts/UserContext';
 import { AuthContext } from '../contexts/AuthContext';
@@ -34,7 +34,7 @@ const Navbar = () => {
                 </div>
                 <div className="dropdown-menu dropdown-menu-lg-right dropdown-secondary"
                   aria-labelledby="navbarDropdownMenuLink-55">
-                  <a className="dropdown-item" href="/profile-image">Profile Image</a>
+                  <a className="dropdown-item" href="/profile-image" data-toggle="modal" data-target="#exampleModal" >Profile Image</a>
                   <a className="dropdown-item" href={`/items-by-user/${userId}`}>My Post</a>
                   <a className="dropdown-item" onClick={logOut} href="/logout">Log Out</a>
                 </div>
@@ -73,6 +73,29 @@ const Navbar = () => {
           {navlist}
         </div>
       </nav>
+
+     
+        <div className="modal fade" id="exampleModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal-dialog" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+              <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div className="modal-body">
+              ...
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+              <button type="button" className="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+        </div>
+      </div>
+     
+
     </div>  
   );
 }
