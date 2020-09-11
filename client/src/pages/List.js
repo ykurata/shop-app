462a64c91538ee6ee197c3cd152bcd7b4fd32b30
ykurata <yasukokurata@gmail.com>
2020-09-11 14:37:18 -0400

    added button on search input

--- client/src/pages/List.js
+++ client/src/pages/List.js
@@ -1,31 +1,31 @@
-import React, { useState, useContext } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useContext } from "react";
+import { Link } from "react-router-dom";
 
 import Navbar from "../components/Navbar";
-import Item from '../components/Item';
-import Loading from '../components/Loading';
-import NoItem from '../components/NoItem';
-import { ItemContext } from '../contexts/ItemContext';
+import Item from "../components/Item";
+import Loading from "../components/Loading";
+import NoItem from "../components/NoItem";
+import { ItemContext } from "../contexts/ItemContext";
 
 const List = () => {
   const { allItems, loading } = useContext(ItemContext);
-  const [search, setSearch] = useState('');
+  const [search, setSearch] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
   const [itemsPerPage] = useState(5);
-  
-  const onChange = e => {
-    setSearch(e.target.value);
-  }
 
-  const handleClick = e => {
+  const onChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const handleClick = (e) => {
     setCurrentPage(Number(e.target.id));
-  }
+  };
 
   const filteredItems = allItems.filter((item) => {
     const query = search.toLowerCase();
     return (
       item.name.toLowerCase().indexOf(query) >= 0 ||
-      item.category.toLowerCase().indexOf(query) >=0
+      item.category.toLowerCase().indexOf(query) >= 0
     );
   });
 
@@ -46,23 +46,35 @@ const List = () => {
   for (let i = 1; i <= Math.ceil(filteredItems.length / itemsPerPage); i++) {
     pageNumbers.push(i);
   }
-    
-  const renderPageNumbers = pageNumbers.map(number => {
+
+  const renderPageNumbers = pageNumbers.map((number) => {
     return (
-      <li key={number} id={number} onClick={handleClick}>{number}</li>
+      <li key={number} id={number} onClick={handleClick}>
+        {number}
+      </li>
     );
   });
 
   let numberOfItems;
   if (filteredItems.length === 0) {
     numberOfItems = null;
-  } else if (filteredItems.length < indexOfLastItem ) {
-    numberOfItems = <p>Showing {indexOfFirstItem + 1} - {filteredItems.length}&nbsp; of &nbsp;{filteredItems.length}&nbsp; items</p>
+  } else if (filteredItems.length < indexOfLastItem) {
+    numberOfItems = (
+      <p>
+        Showing {indexOfFirstItem + 1} - {filteredItems.length}&nbsp; of &nbsp;
+        {filteredItems.length}&nbsp; items
+      </p>
+    );
   } else if (filteredItems.length > 5) {
-    numberOfItems = <p>Showing {indexOfFirstItem + 1} - {indexOfLastItem}&nbsp; of &nbsp;{filteredItems.length}&nbsp; items</p>
+    numberOfItems = (
+      <p>
+        Showing {indexOfFirstItem + 1} - {indexOfLastItem}&nbsp; of &nbsp;
+        {filteredItems.length}&nbsp; items
+      </p>
+    );
   } else if (filteredItems.length < 5) {
-    numberOfItems = <p>Showing {filteredItems.length} items</p>
-  } 
+    numberOfItems = <p>Showing {filteredItems.length} items</p>;
+  }
 
   return (
     <div>
@@ -71,21 +83,35 @@ const List = () => {
       <div className="container-fluid item-list">
         <div className="row">
           <div className="col-lg-3 col-md-3"></div>
-          <div className="col-lg-9 col-md-9">
-            {numberOfItems}
-          </div>
+          <div className="col-lg-9 col-md-9">{numberOfItems}</div>
         </div>
         <div className="row list-outer">
-           {/* Search input and select form category */}
+          {/* Search input and select form category */}
           <div className="col-lg-3 col-md-3">
             <div className="input-group md-form form-sm form-1 pl-0 mb-5 search-form">
-              <div className="input-group-prepend">
-                <span className="input-group-text purple lighten-3" id="basic-text1"><i className="fas fa-search text-white"
-                    aria-hidden="true"></i></span>
+              <input
+                onChange={onChange}
+                name="search"
+                value={search}
+                className="form-control my-0 py-1"
+                type="text"
+                placeholder="Search item..."
+                aria-label="Search"
+              />
+              <div className="input-group-append">
+                <button className="btn btn-primary" type="button">
+                  <i
+                    className="fas fa-search text-white"
+                    aria-hidden="true"
+                  ></i>
+                </button>
               </div>
-              <input onChange={onChange} name="search" value={search} className="form-control my-0 py-1" type="text" placeholder="Search item..." aria-label="Search" />
             </div>
-            <select onChange={onChange} name="search" className="browser-default custom-select mb-5">
+            <select
+              onChange={onChange}
+              name="search"
+              className="browser-default custom-select mb-5"
+            >
               <option value="">Categories</option>
               <option value="Phone/Laptop">Phone/Laptop</option>
               <option value="Clothing">Clothing</option>
@@ -94,22 +120,16 @@ const List = () => {
               <option value="Furniture">Furniture</option>
               <option value="Camera">Camera</option>
             </select>
-          </div>  
+          </div>
           <div className="col-lg-9 col-md-9">
             <div className="list-group">
               {/* display message if there is no items  */}
               {filteredItems.length === 0 && loading === true ? (
                 <NoItem />
-              ) : (
-                null
-              )}
+              ) : null}
 
               {/* Loading Message */}
-              {loading === false ? (
-                <Loading />
-              ) : (
-                null
-              )}
+              {loading === false ? <Loading /> : null}
 
               {card}
             </div>
@@ -119,10 +139,10 @@ const List = () => {
           <ul className="pagination justify-content-center">
             {renderPageNumbers}
           </ul>
-        </div>  
+        </div>
       </div>
     </div>
   );
-}
+};
 
 export default List;
